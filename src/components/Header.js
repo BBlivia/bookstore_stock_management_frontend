8a262a8554eab2a69e13d3ac94eb2c64b9c08579
Olivia Bloway <blowayol@msu.edu>
2023-01-01 13:24:49 -0500

    added Header comp

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+
+
+export default function Header(){
+    return(
+        <>
+        <header>
+            <nav className='nav navbar-expand-md navbar-dark bg-primary p-3'>
+                <div>
+                    <a className="navbar-brand m-5" href="#">Book Management Stystem</a>
+                </div>
+            </nav>
+        </header>
+           
+        </>
+    )
+}
